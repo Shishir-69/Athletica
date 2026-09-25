5b45c143486a4214c84b6b106a67f2dcaa3a0b2b
ShreeharshaG <shreeharsha8888@gmail.com>
2026-09-25 12:06:10 +0530

    set user model

---
+++ backend/src/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['student', 'teacher', 'community'],
+      default: 'student',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
+export { User };
